684b1a117e422d6acde7749b76dc7b8460a2af32
victoria <victoriadelfina1998@hotmail.com>
2020-08-27 01:38:07 -0300

    cambios

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,31 +8,72 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import songs from "./songs"
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+
+const Playlist =() => {
+
+  let canciones = [];
+
+ // const classes = useStyles();
+//Resultados de la busqueda
+ const [songsResult,setSongsResult] = React.useState ([]);
+ //songsResult->La cancion que coincide con la busqueda
+ //setSongResult -> La funcion que permite guardar la coincidencia
+
+const [searchSong,setSearchSong] = React.useState(""); 
+//searchSong->es la cancion buscada(ingreso el usuario)
+//setSearchSong->es la funcion que permite guardar la cancio buscada
+
+
+///Ejecuta cuando el input cambia
+//Agarra el valor(lo que escribio el usuario)
+//Y lo guarda(setSearchSong)
+const valorChange =(Input)=>{ 
+  setSearchSong (Input.target.value);
+}
+//Se ejecuta cuando le doy a una tecla en el input
+const summitSearch=(Input)=>{
+  //Valido si la tecla fue el enter
+  if(Input.key == "Enter") {
+    let resultado = songs.find(s=> s.name == searchSong) 
+    if(resultado){
+      setSongsResult([resultado])
+    }
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
-export default function SimpleTable() {
-  const classes = useStyles();
 
+
+}
   return (
+    <Fragment> 
+      <Grid container="bool" justify="center" alignItems="center" direction="column"></Grid>
+      <Box m={5}>
+        <FormControl>
+      <InputLabel htmlFor="input-with-icon-adornment">Buscar</InputLabel>
+      <Input onChange= {valorChange}  onKeyPress= {summitSearch}
+        id="input-with-icon-adornment"
+        startAdornment={
+        <InputAdornment position="start">
+            <AccountCircle />
+          </InputAdornment>
+        }
+      />
+      </FormControl>
+      </Box>
+    
+
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+      <Table aria-label="simple table">
         <TableHead>
           <TableRow>
           <TableCell align="left">Nombre</TableCell>
@@ -43,20 +84,23 @@ export default function SimpleTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {songsResult.map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="left">Nombre</TableCell>
-                <TableCell align="left">Artista</TableCell>
-                <TableCell align="left">Album</TableCell>
-                <TableCell align="left">Duracion</TableCell>
+            <TableCell align="left">{row.name}</TableCell>
+                <TableCell align="left">{row.artist.name}</TableCell>
+                <TableCell align="left">{row.album}</TableCell>
+              <TableCell align="left">{row.duration}</TableCell>
                 <TableCell align="left">Agregar</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
+    </Fragment>
   );
 }
+
+export default Playlist;
